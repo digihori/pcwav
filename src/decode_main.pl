4ae4d4e9952f17f8623664b979ba13c9a22927e4
digihori <yo6987@gmail.com>
2026-03-29 16:48:14 +0900

    update

--- src/decode_main.pl
+++ src/decode_main.pl
@@ -2,12 +2,91 @@ use strict;
 use warnings;
 use FindBin;
 use lib "$FindBin::Bin";
+use lib "$FindBin::Bin/PCWAV";
+use lib "$FindBin::Bin/..";
+use PCWAV::Common;
+use PCWAV::WavReader;
+use PCWAV::PcmNormalize;
+use PCWAV::RawDecode;
+use PCWAV::Binary::S1Decode;
 
-die <<'MSG';
-decode side is not implemented yet.
-
-planned subcommands:
+sub usage {
+    die <<'MSG';
+usage:
   perl src/decode_main.pl raw input.wav output.bin
-  perl src/decode_main.pl bin --format s1 input.wav output.bin
-  perl src/decode_main.pl basic --format s1 input.wav output.bas
+  perl src/decode_main.pl s1bin [--skip-ms 1000] [--skip-samples N] input.wav output.bin
 MSG
+}
+
+sub decode_raw {
+    my ($input, $output) = @_;
+    my $wav = PCWAV::WavReader::read_wav($input);
+    my $pcm = PCWAV::PcmNormalize::normalize_to_mono_s16($wav);
+    my @bytes = PCWAV::RawDecode::decode_samples_to_bytes(
+        $pcm->{sample_rate},
+        $pcm->{samples},
+    );
+    my $data = join '', map { chr($_) } @bytes;
+    PCWAV::Common::write_file_bin($output, $data);
+    print "wrote $output\n";
+}
+
+sub decode_s1bin {
+    my (@args) = @_;
+    my %opt = (skip_ms => 1000, skip_samples => 0);
+
+    while (@args > 2) {
+        my $k = shift @args;
+        if ($k eq '--skip-ms') {
+            $opt{skip_ms} = PCWAV::Common::parse_num(shift @args);
+        } elsif ($k eq '--skip-samples') {
+            $opt{skip_samples} = PCWAV::Common::parse_num(shift @args);
+        } else {
+            die "unknown option: $k\n";
+        }
+    }
+
+    usage() unless @args == 2;
+    my ($input, $output) = @args;
+
+    my $wav = PCWAV::WavReader::read_wav($input);
+    my $pcm = PCWAV::PcmNormalize::normalize_to_mono_s16($wav);
+
+    my @samples = @{$pcm->{samples}};
+    my $skip_samples = $opt{skip_samples} || int($pcm->{sample_rate} * ($opt{skip_ms} / 1000.0));
+    if ($skip_samples > 0) {
+        die "skip exceeds sample count\n" if $skip_samples >= @samples;
+        @samples = @samples[$skip_samples .. $#samples];
+    }
+
+    my @raw = PCWAV::RawDecode::decode_samples_to_bytes(
+        $pcm->{sample_rate},
+        \@samples,
+    );
+    my $info = PCWAV::Binary::S1Decode::find_and_extract(\@raw);
+    my $data = join '', map { chr($_) } @{$info->{body_bytes}};
+    PCWAV::Common::write_file_bin($output, $data);
+
+    print "wrote $output\n";
+    printf "offset: %d\n", $info->{offset};
+    printf "name: %s\n", $info->{name};
+    printf "addr: %04X\n", $info->{addr};
+    printf "length: %d\n", $info->{length};
+}
+
+sub main {
+    my @args = @ARGV;
+    usage() unless @args >= 3;
+    my $mode = shift @args;
+
+    if ($mode eq 'raw') {
+        usage() unless @args == 2;
+        decode_raw(@args);
+    } elsif ($mode eq 's1bin') {
+        decode_s1bin(@args);
+    } else {
+        die "unknown mode: $mode\n";
+    }
+}
+
+main();
