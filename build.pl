bb75855c1a66fd5053ae9045ed75f944592f7773
digihori <yo6987@gmail.com>
2026-03-29 16:53:43 +0900

    update

--- build.pl
+++ build.pl
@@ -9,14 +9,17 @@ my %targets = (
             'src/PCWAV/Common.pm',
             'src/PCWAV/WavWriter.pm',
             'src/PCWAV/Format/S1.pm',
-            'src/PCWAV/Binary/S1.pm',
-            'src/PCWAV/Basic/S1.pm',
+            'src/PCWAV/Binary/S1Encode.pm',
         ],
         main => 'src/encode_main.pl',
     },
     'dist/pcwav-decode.pl' => {
         modules => [
             'src/PCWAV/Common.pm',
+            'src/PCWAV/WavReader.pm',
+            'src/PCWAV/PcmNormalize.pm',
+            'src/PCWAV/RawDecode.pm',
+            'src/PCWAV/Binary/S1Decode.pm',
         ],
         main => 'src/decode_main.pl',
     },
@@ -39,6 +42,7 @@ sub build_one {
         my $text = slurp($file);
         $text =~ s/^\s*use\s+strict\s*;\s*$//mg;
         $text =~ s/^\s*use\s+warnings\s*;\s*$//mg;
+        $text =~ s/^\s*use\s+PCWAV::[A-Za-z0-9_:]+\s*;\s*$//mg;
 
         print {$fh} "# ===== BEGIN $file =====\n";
         print {$fh} $text;
@@ -50,6 +54,7 @@ sub build_one {
     $main =~ s/^\s*use\s+warnings\s*;\s*$//mg;
     $main =~ s/^\s*use\s+FindBin\b.*?;\s*$//mg;
     $main =~ s/^\s*use\s+lib\b.*?;\s*$//mg;
+    $main =~ s/^\s*use\s+PCWAV::[A-Za-z0-9_:]+\s*;\s*$//mg;
 
     print {$fh} "package main;\n\n";
     print {$fh} "# ===== BEGIN $spec->{main} =====\n";
